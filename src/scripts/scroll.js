ddd78a90d16dab593d2aa474cc5f47b7eca44491
Airath <narumi3992@gmail.com>
2025-05-31 14:58:35 +0200

    proba

---
+++ src/scripts/scroll.js
@@ -0,0 +1,10 @@
+// // public/scroll.js vagy src/scripts/scroll.js
+// window.addEventListener("scroll", () => {
+//   const header = document.getElementById("site-header");
+
+//   if (window.scrollY > 10) {
+//     header.classList.add("scrolled");
+//   } else {
+//     header.classList.remove("scrolled");
+//   }
+// });
